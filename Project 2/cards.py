64263f6349863cca98e7d4f771c57a3ba2e0b115
Luciana Nobrega <luciananobrega93@gmail.com>
2020-05-30 15:11:05 -0400

    Organizing Deck and Cards classes

--- Project 2/cards.py
+++ Project 2/cards.py
@@ -1,28 +1,39 @@
 import random
 
-class Cards:
-    def __init__(self):
-        self.all_cards = ['A']
-        for i in range(1,11):
-            self.all_cards.append(str(i))
-        self.all_cards.append('J')
-        self.all_cards.append('Q')
-        self.all_cards.append('K')
-        self.values = { 'A': 11, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ['A']
+for i in range(1,11):
+    ranks.append(str(i))
+ranks.append('J')
+ranks.append('Q')
+ranks.append('K')
+
+values = { 'A': 11, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
                         '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10 
-                     }
+         }
 
-class Deck(Cards):
+class Cards:
+    def __init__(self, ranks, suits):
+        self.ranks = ranks
+        self.suits = suits
+    
+    def __str__(self):
+        return self.ranks + ' of ' + self.suits
+
+class Deck():
     def __init__(self):
-        Cards.__init__(self)
-
+        self.deck = []
+        for rank in ranks:
+            for suit in suits:
+                self.deck.append(Cards(rank, suit))
     def get_card(self):
         """
         Randomly chooses a card and remove it from the deck
         """
-        random_card_index = random.randint(0, len(self.all_cards) - 1)
-        card = self.all_cards.pop(random_card_index)
+        random_card_index = random.randint(0, len(self.deck) - 1)
+        card = self.deck.pop(random_card_index)
         return card
+
 
 class Hand(Cards):
     def __init__(self):
